d3c92422d4a89c921c86f16406e3a2771c24950b
Daniel Agyeman <daniel_agyeman@msn.com>
2024-09-19 16:51:14 +0100

    feat(ui): install rive and add wifi animation to network connection modal

--- src/core/network-info.tsx
+++ src/core/network-info.tsx
@@ -1,21 +1,23 @@
+import { addEventListener } from '@react-native-community/netinfo';
 import React from 'react';
 
+import { NetworkConnectionModal } from '@/components/network-connection-modal';
 import { Button, View } from '@/ui';
-import { Options, useModal } from '@/ui';
+import { useModal } from '@/ui';
 
 export function NetworkInfo() {
   const modal = useModal();
+  // Subscribe
+  const unsubscribe = addEventListener((state) => {
+    console.log('Connection type', state.type);
+    console.log('Is connected?', state.isConnected);
+  });
+
+  unsubscribe();
   return (
     <View className="absolute inset-x-0 bottom-60 z-50">
       <Button label="test" variant="ghost" onPress={modal.present} />
-      <Options
-        ref={modal.ref}
-        options={[
-          { label: '3', value: 'en' },
-          { label: '4', value: 'ar' },
-        ]}
-        onSelect={() => {}}
-      />
+      <NetworkConnectionModal ref={modal.ref} />
     </View>
   );
 }
